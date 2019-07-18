292efb9a2e7340aac233808e57496b2a3e46977f
skarpov <sergo-8ck@yandex.ru>
2019-07-18 16:21:46 +0300

    Methods for Department Model

--- app/Department.php
+++ app/Department.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Department extends Model
 {
+    use Sluggable;
+
     protected $fillable = ['name', 'description'];
 
     /**
@@ -21,5 +25,99 @@ class Department extends Model
             'department_id',
             'user_id'
         );
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    /**
+     * Add Department
+     *
+     * @param $fields
+     *
+     * @return Department
+     */
+    public static function add($fields)
+    {
+        $post = new static;
+        $post->fill($fields);
+        $post->save();
+
+        return $post;
+    }
+
+    /**
+     * Edit Department
+     *
+     * @param $fields
+     */
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
+    }
+
+    /**
+     * Remove Department
+     *
+     * @throws \Exception
+     */
+    public function remove()
+    {
+        $this->removeLogo();
+        $this->delete();
+    }
+
+    /**
+     * Remove Logo
+     */
+    public function removeLogo()
+    {
+        if($this->logo != null)
+        {
+            Storage::delete('logo/' . $this->logo);
+        }
+    }
+
+    /**
+     * Upload Logo
+     *
+     * @param $logo
+     */
+    public function uploadLogo($logo)
+    {
+        if($logo == null) { return; }
+
+        $this->removeLogo();
+        $filename = str_random(10) . '.' . $logo->extension();
+        $logo->storeAs('logo', $filename);
+        $this->logo = $filename;
+        $this->save();
+    }
+
+    /**
+     * Get Logo
+     *
+     * @return string
+     */
+    public function getLogo()
+    {
+        if($this->logo == null)
+        {
+            return '/logo/no-image.png';
+        }
+
+        return '/logo/' . $this->logo;
     }
 }
